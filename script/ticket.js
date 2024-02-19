117f28a2460e764e9ab036f714dad754916c380b
akbor shanto <akborshanto2002@gmail.com>
2024-02-19 00:37:30 -0800

    ANPORIBOHON

--- script/ticket.js
+++ script/ticket.js
@@ -1,6 +1,10 @@
 /* function-select-ticket============================*/
-
+let count=0;
 let addSelect = document.getElementsByClassName("add-select");
+
+/* one-cliclk */
+
+
 /* count-plus-minus */
 let countPlus = 0;
 let countMinus = 40;
@@ -11,12 +15,25 @@ let perTicketTaka = 0;
 
 /* POribohohon-MAINPART==================================== */
 for (let addSelects of addSelect) {
+
+
+ 
+
+
+
   /* listener */
   addSelects.addEventListener("click", function (e) {
-    /* select-bg */
+
+e.target.setAttribute("disabled", true);
+    // for( let i=0; i<= 4; i++){
+    //   document.getElementById("addSelect").disabled = true;
+    // }
+
+
+
+
     const addColor = addSelects.classList.add("bg-green-500");
 
-    
     /* count-minus and plus */
 
     if (countMinus > 0) {
@@ -24,30 +41,40 @@ for (let addSelects of addSelect) {
       const countMinusDisplay = (document.getElementById(
         "seat-count-minus"
       ).innerText = countMinus);
+      
     }
+
+
+
+
     countPlus += 1;
     const countPlusDisplay = (document.getElementById(
       "seat-plus-count"
     ).innerText = countPlus);
+
+
+
+
+
     /* opt */
-    if (countPlus > 40 ) {
+    if (countPlus > 40) {
       countPlus += 1;
-     // alert("TICKET FULL");
+      // alert("TICKET FULL");
       const countPlusDisplay = (document.getElementById(
         "seat-plus-count"
       ).innerText = countPlus);
     }
-/* bg-remove */
-if( countPlus > 4  ){
-   
-    const addColor = addSelects.classList.remove("bg-green-500");
-  //  const disabled=addSelects.classList.add('disabled')
-  //  document.getElementById("addSelect").disabled = true;
 
-
-}
-
-
+    /* bg-remove */
+    if (countPlus > 4) {
+      const addColor = addSelects.classList.remove("bg-green-500");
+      // const disabled=addSelects.classList.add('disabled')
+     //  document.getElementById("addSelect").disabled = true;
+    document.getElementById("addSelect").setAttribute("disabled",true);
+    //removeEventListener('addSelects')
+   // e.target.setAttribute("disabled",true)
+    
+    }
 
     /* dynamic-seat-count */
     const addSelectss = addSelects.innerText;
@@ -67,24 +94,20 @@ if( countPlus > 4  ){
 
     /* Price-calculate ===== somossa ============*/
 
-    perTicketTaka += 550;
-
-
-
+    const totalTaka= perTicketTaka += 550;
 
     /* total-taka DISPLAy */
     const totalTakaDisplay = (document.getElementById("total-taka").innerText =
-      perTicketTaka);
+    totalTaka);
     let grandTotalDisplay = (document.getElementById("grand-taka").innerText =
-      perTicketTaka);
+    totalTaka);
 
     /* grans-taka */
 
 
-  
+
 
   });
-
 
 
 
@@ -93,37 +116,17 @@ if( countPlus > 4  ){
 /* =============================================================== */
 /* next-button */
 
-
-
 /* coupone-input */
 const couponButton = document.getElementById("coupon-button");
 
 couponButton.addEventListener("click", function () {
   let couponInput = document.getElementById("coupon-input").value.toUpperCase();
 
-  if (couponInput === "NEW15" || couponInput === "COUPLE 20" ) {
-
-
-
-
-
+  if (couponInput === "NEW15" || couponInput === "COUPLE 20") {
     document.getElementById("coupon-input").classList.add("hidden");
     couponButton.classList.add("hidden");
-  }else{
-
-  
-    alert("invalid coupon")
-    document.getElementById("coupon-input").value = ""
-  } 
-
-
-
-
-
-
+  } else {
+    alert("invalid coupon");
+    document.getElementById("coupon-input").value = "";
+  }
 });
-
-
-   
-
-
